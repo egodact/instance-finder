6fbeb48e66282594e0fc960146306adc40ba3919
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2019-06-16 20:02:35 +0200

    Added max height to school list on desktop-ish devices

--- src/components/InstanceFinder/SchoolPicker/SchoolList.js
+++ src/components/InstanceFinder/SchoolPicker/SchoolList.js
@@ -1,12 +1,19 @@
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
 import { grey } from '../../../styles/colors';
+import { mediaQuery } from '../../../styles/utils';
 
 const SchoolList = styled('section')({
+  maxHeight: 240,
+  overflowY: 'auto',
   border: `2px solid ${grey}`,
   borderTop: 0,
   borderBottomLeftRadius: 4,
-  borderBottomRightRadius: 4
+  borderBottomRightRadius: 4,
+  [mediaQuery(640)]: {
+    maxHeight: 'none',
+    overflowY: 'initial'
+  }
 }, ({ open }) => {
   if (!open) {
     return {
